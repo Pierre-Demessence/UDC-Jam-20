688214bfc393fdc458eabdffe8937b343db356c5
Pierre Demessence <pierre.demessence@gmail.com>
2021-12-05 07:36:04 +0100

    Moving background and jump

--- Assets/Jump.cs
+++ Assets/Jump.cs
@@ -1,18 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Jump : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody2D _rigidbody2D;
+    [SerializeField] private float _jumpForce = 10;
+
+    private void Start()
     {
-        
+        _rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnJump()
     {
-        
+        DoJump();
+    }
+
+    private void DoJump()
+    {
+        _rigidbody2D.AddRelativeForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
     }
 }
