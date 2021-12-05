b52aa05645f68eaca0410aa0f9b30918887b98a4
Pierre Demessence <pierre.demessence@gmail.com>
2021-12-05 17:47:39 +0100

    Character can now only jump when grounded

--- Assets/Jump.cs
+++ Assets/Jump.cs
@@ -1,16 +1,25 @@
-using System;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class Jump : MonoBehaviour
 {
-    private Rigidbody2D _rigidbody2D;
     [SerializeField] private float _jumpForce = 10;
+    private Rigidbody2D _rigidbody2D;
+    private bool _isGrounded;
 
     private void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground")) _isGrounded = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground")) _isGrounded = false;
     }
 
     public void OnJump()
@@ -20,6 +29,7 @@ public class Jump : MonoBehaviour
 
     private void DoJump()
     {
+        if (!_isGrounded) return;
         _rigidbody2D.AddRelativeForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
     }
 }
